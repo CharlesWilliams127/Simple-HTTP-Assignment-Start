1ccb4c42e76803294886dbbb47b9045a2e09668f
Charles Williams (RIT Student) <crw4147@ad.rit.edu>
2018-09-04 18:36:22 -0400

    Finished base assignment

--- src/server.js
+++ src/server.js
@@ -1,4 +1,38 @@
 const http = require('http');
 const htmlHandler = require('./htmlResponse');
-cosnt textHandler = require('./textResponse');
-cosnt jsonHandler = require('./jsonResponse');
+const textHandler = require('./textResponse');
+const jsonHandler = require('./jsonResponse');
+
+const port = process.env.port || process.env.NODE_PORT || 3000;
+
+const onRequest = (request, response) => {
+  console.log(request.url);
+
+  switch (request.url) {
+    case '/':
+      htmlHandler.getIndex(request, response);
+      break;
+    case '/page2':
+      htmlHandler.getPage2(request, response);
+      break;
+    case '/hello':
+      textHandler.getHello(request, response);
+      break;
+    case '/time':
+      textHandler.getTime(request, response);
+      break;
+    case '/helloJSON':
+      jsonHandler.getHelloJSON(request, response);
+      break;
+    case '/timeJSON':
+      jsonHandler.getTimeJSON(request, response);
+      break;
+    default:
+      htmlHandler.getIndex(request, response);
+      break;
+  }
+};
+
+http.createServer(onRequest).listen(port);
+
+console.log(`listening on ${port}`);
